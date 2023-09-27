b3a6335d11cb6be8d88632913c6587d7def2f630
eric.chen <eric.chen@athena.com.tw>
2023-09-27 13:07:51 +0800

    fix: code tag style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,47 @@ module.exports = {
   ],
   darkMode: ["class"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            code: {
+              color: '#111b27',
+              backgroundColor: '#e3eaf2',
+              padding: '0 0.25rem',
+              borderRadius: '0.25rem'
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            'a code': {
+              color: '#111b27',
+            },
+            'h1 code': {
+              color: '#111b27',
+            },
+            'h2 code': {
+              color: '#111b27',
+            },
+            'h3 code': {
+              color: '#111b27',
+            },
+            'h4 code': {
+              color: '#111b27',
+            },
+            'blockquote code': {
+              color: '#111b27',
+            },
+            'thead th code': {
+              color: '#111b27',
+            },
+          }
+        }
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
